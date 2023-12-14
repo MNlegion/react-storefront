4b8f21fec362868c38a88aa5a0a62d47d30943fb
MNlegion <rkreuser30@gmail.com>
2023-12-14 17:41:47 -0500

    User model setup

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,49 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     // Schema Definition
-
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        minlength: [3, "Username must be at least 3 characters long"],
+        unique: [true, "Username must be unique"],
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        validate: {
+            validator: val => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val),
+            message: "Please enter a valid email",
+        },
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [8, "Password must be at least 8 characters long"],
+    },
+    // Additional fields like addresses, phone, etc. can be added here
+    createAt: {
+        type: Date,
+        default: Date.now,
+    }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+// Hash the password before saving the user model
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
