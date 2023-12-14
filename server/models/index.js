bd09c9e69099f0eb4f3a6c4fdcebd0be2e8ff23c
MNlegion <rkreuser30@gmail.com>
2023-12-13 23:13:42 -0500

    initial UserSchema setup

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,16 @@
 const User = require('./User');
-
+const Product = require('./Product');
+const Order = require('./Order');
+const Cart = require('./Cart');
+const Category = require('./Category');
+const Wishlist = require('./Wishlist');
 
 
 module.exports = {
-    User
+    User,
+    Product,
+    Order,
+    Cart,
+    Category,
+    Wishlist,
 };
