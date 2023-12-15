5626188bb70619ce6b67478e143e8de869938512
MNlegion <rkreuser30@gmail.com>
2023-12-14 20:44:46 -0500

    Cart model setup

--- server/models/Cart.js
+++ server/models/Cart.js
@@ -1,8 +1,35 @@
 const mongoose = require('mongoose');
 
-const CartSchema = new mongoose.Schema({
-    // Schema Definition
-
+const cartSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    items: [{
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product',
+            required: true
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            min: [1, 'Quantity cannot be less than 1.']
+        }
+    }],
+    totalPrice: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+    // Additional fields like cart status can be added if needed
 });
 
-module.exports = mongoose.model('Cart', CartSchema);
+const Cart = mongoose.model('Cart', cartSchema);
+
+module.exports = Cart;
