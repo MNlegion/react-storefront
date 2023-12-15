78ee7293fc72f2d76d6bbee34810b80b1b46b305
MNlegion <rkreuser30@gmail.com>
2023-12-14 21:13:26 -0500

    wishlist model setup

--- server/models/Wishlist.js
+++ server/models/Wishlist.js
@@ -1,8 +1,22 @@
 const mongoose = require('mongoose');
 
-const WishlistSchema = new mongoose.Schema({
-    // Schema Definition
-
+const wishlistSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    items: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    }],
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+    // Additional fields can be added if needed
 });
 
-module.exports = mongoose.model('Wishlist', WishlistSchema);
+const Wishlist = mongoose.model('Wishlist', wishlistSchema);
+
+module.exports = Wishlist;
